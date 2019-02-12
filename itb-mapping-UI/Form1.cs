11a4cb44418bf54aa2e079a1ed7ae4a98c1ec3a0
duduru <duduru@DESKTOP-6VR16NN>
2019-02-12 17:51:44 +0800

    Add media player again = =

--- itb-mapping-UI/Form1.cs
+++ itb-mapping-UI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;                //讀檔
 
 namespace itb_mapping_UI
 {
@@ -15,6 +16,45 @@ namespace itb_mapping_UI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //選擇 AVI
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //選擇 ITB.csv
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox2.Text = openFileDialog2.FileName;
+            }
+
+            StreamReader sr = new StreamReader(path: textBox2.Text);
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] item = line.Split(',');
+            }
+            sr.Close();
         }
     }
 }
